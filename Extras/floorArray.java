255a7e0112124fc7ea6bf0928ef0163eb7c79228
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-02-23 15:52:59 +0530

    Add File

--- Extras/floorArray.java
+++ Extras/floorArray.java
@@ -3,7 +3,7 @@
 
 import java.util.Scanner;
 
-public class floorArray {
+public class FloorArray {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the number of elements: ");
