e58bbf1e68a75167e775fb269e237a5d2049ad24
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-04-03 23:25:20 +0530

    Add File

---
+++ Abstraction/divisorSumInterface.java
@@ -0,0 +1,16 @@
+package abstraction;
+import java.util.Scanner;
+
+public class divisorSumInterface {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value of n: ");
+		int n = sc.nextInt();
+		if(n > 1000) {
+			System.out.println("n value beyond the limit");
+			System.exit(0);
+		}
+		MyCalculator m1 = new MyCalculator();
+		System.out.println("Sum of divisors of " + n + ": " + m1.divisor_sum(n));
+	}
+}
