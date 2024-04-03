5aec519390fbcb34be39b125b53c6169bad14beb
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-04-03 23:37:05 +0530

    Update File

--- Abstraction/myCalculator.java
+++ Abstraction/myCalculator.java
@@ -1,3 +1,5 @@
+/* Create a interface AdvancedArithmetic which contains a method signature int divisor_sum(int n). Write a class called My Calculator which implements the 
+interface  divisor_sum function just takes an integer as input and return the sum of all its divisors.*/
 interface AdvancedArithmetic{
 	int divisor_sum(int n);
 }
