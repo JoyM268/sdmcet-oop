05cbaa8fbec14058eea86a4334958924e8341a34
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-04-03 23:31:19 +0530

    Add File

---
+++ Abstraction/myCalculator.java
@@ -0,0 +1,17 @@
+interface AdvancedArithmetic{
+	int divisor_sum(int n);
+}
+
+public class myCalculator implements AdvancedArithmetic{
+	public int divisor_sum(int n) {
+		int sum = 0;
+		for(int i = 1; i <= n; i++) {
+			if(n % i == 0) {
+				sum += i;
+			}else {
+				continue;
+			}
+		}
+		return sum;
+	}
+}
