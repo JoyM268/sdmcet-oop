46753987ce72f1cc51d2e6ebd6fa176a918b29f4
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-04-25 06:49:01 +0530

    Add File

---
+++ Exception Handling/checkPalindrome.java
@@ -0,0 +1,28 @@
+// Write a Java Program that takes a string as command-line argument and checks whether the string is palindrome or not.
+
+class notPalindrome extends Exception{
+    String s;
+    notPalindrome(String s){
+        this.s = s;
+    }
+
+    public String toString(){
+        return s + " is not a palindrome";
+    }
+}
+
+class checkPalindrome{
+    public static void main(String[] args) {
+        String str = args[0];
+        StringBuilder s1 = new StringBuilder(str);
+        if(str.equals(s1.reverse().toString())){
+            System.out.println(str + " is a palindrome");
+        }else{
+            try {
+                throw new notPalindrome(str);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
