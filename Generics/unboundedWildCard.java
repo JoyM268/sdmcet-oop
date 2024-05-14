662c7b08ea88ac349aa6003fc33d5f565e7f8a32
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-05-14 15:51:41 +0530

    Add File

---
+++ Generics/unboundedWildCard.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+class unboundedWildCard {
+    public static void main(String[] args){
+		//Integer List
+		List<Integer> list1= Arrays.asList(1,2,3);
+
+		//Double list
+		List<Double> list2=Arrays.asList(1.1,2.2,3.3);
+		printlist(list1);
+		printlist(list2);
+	}
+
+	private static void printlist(List<?> list){
+		System.out.println(list);
+	}
+}
---
+++ Practice Programs/Practice Programs - 4/Q1.java
@@ -0,0 +1,39 @@
+/* Write a Java program that provides a generic sort method that can sort elements of any reference type.
+[Hint: use compareTo() method available in Comparable<T> interface] */
+public class Q1{
+    static <T> void display(T []arr){
+        for(T e : arr){
+            System.out.println(e);
+        }
+    }   
+    
+    static <T extends Comparable<T>> void arrSort(T []arr){
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = 0; j < arr.length - i - 1; j++){
+                if(arr[j].compareTo(arr[j + 1]) > 0){
+                    T temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }   
+    
+    public static void main(String[] args) {
+
+        Integer[] a1 = {5, 6, 2, 1};
+        Character[] c1 = {'c', 's', 'e'}; 
+
+        System.out.println("Int Array before sort:");
+        display(a1);       
+        arrSort(a1);
+        System.out.println("Int Array after sort:");
+        display(a1);
+
+        System.out.println("Char Array before sort:");
+        display(c1);       
+        arrSort(c1);
+        System.out.println("Char Array after sort:");
+        display(c1);
+    }
+}
