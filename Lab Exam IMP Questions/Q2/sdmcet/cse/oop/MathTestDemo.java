37c8b4ab94393b3a16497425e32cf2082a46edc9
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-05-31 06:22:46 +0530

    Add File

---
+++ Lab Exam IMP Questions/Q2/sdmcet/cse/oop/MathTestDemo.java
@@ -0,0 +1,10 @@
+/* Using the concept of packages, implement the following scenario:
+Create a package named “basicMath” inside “sdmcet.cse.oop” package.
+It has a class named Basic and provides following methods: 
+i) Checks if a number is prime or not 
+ii) Finds sum of digits of a number Create another package called “advancedMath” inside “sdmcet.cse.oop” package. 
+It has a class named Advanced and provides following methods: 
+i) Calculates sine, cosine & tan of a degree 
+ii) Finds the sum of primary diagonal elements of a 3X3 matrix 
+Create a class named MathTestDemo defined inside “sdmcet.cse.oop” package.
+This class has main() method and must invoke all the methods of Basic and Advanced classes. */
