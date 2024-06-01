029420c8a7d1f298b47b14c0225af09a242e74da
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-06-02 00:25:09 +0530

    Add File

--- Lab Exam IMP Questions/Q4.java
+++ Lab Exam IMP Questions/Q4.java
@@ -3,6 +3,8 @@ to install an "Automated Manufacturing and Maintenance System". One important ta
 furnace and monitor its temperature. If the temperature of the furnace rises above 300 ⁰C, then it should immediately generate an alarm.
 It should also generate a separate alarm if the temperature of the furnace falls below 100 ⁰C. You are hired as a Java Programmer to develop this system. 
 Write a Java Program which simulates the above scenario using exception handling mechanism. */
+import java.util.Scanner;
+
 class highTemperatureException extends Exception{
     public String toString(){
         return "The temperature of furnace is above 300 C";
@@ -17,7 +19,10 @@ class lowTemperatureException extends Exception{
 
 public class Q4 {
     public static void main(String[] args) {
-        int temperature = Integer.parseInt(args[0]);
+        Scanner sc = new Scanner(System.in);
+        //int temperature = Integer.parseInt(args[0]);
+        System.out.print("Enter the temperature: ");
+        int temperature = sc.nextInt();
         try{
             if(temperature > 300){
                 throw new highTemperatureException();
