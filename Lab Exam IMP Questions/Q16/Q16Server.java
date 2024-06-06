1c9c85a1fefdadaaf79db364982922c25766ef0e
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-06-06 20:14:39 +0530

    Update File

--- Lab Exam IMP Questions/Q16/Q16Server.java
+++ Lab Exam IMP Questions/Q16/Q16Server.java
@@ -10,7 +10,9 @@ class Q16Server {
     public static void main(String[] args) {
         try {
             ServerSocket ss = new ServerSocket(6000);
+            System.out.println("Waiting for client...");
             Socket s = ss.accept();
+            System.out.println("Connected to client");
             DataInputStream din = new DataInputStream(s.getInputStream());
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 
--- Lab Exam IMP Questions/Q17.java
+++ Lab Exam IMP Questions/Q17.java
@@ -19,7 +19,7 @@ class Q17{
             fr.close();
             fw.close();
         } catch (Exception e) {
-            System.out.println("File not found or file cannot be opened");
+            System.out.println("The files entered are invaild are cannot be opened");
         }      
     }
 }
