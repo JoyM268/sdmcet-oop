55624dafc65dfd6e295d335cbbecfaf1dfc02bb5
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-06-07 20:37:45 +0530

    Add File

---
+++ Java Networking/DSender.java
@@ -0,0 +1,15 @@
+//Program to send message between sender and receiver using UDP(Connectionless)
+//Sender program
+import java.net.*;
+
+public class DSender {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket ds = new DatagramSocket();
+        String str = "Welcome java";
+        InetAddress ip = InetAddress.getByName("127.0.0.1");
+
+        DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ip, 3000);
+        ds.send(dp);
+        ds.close();
+    }
+}
