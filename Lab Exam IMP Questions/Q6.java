98429bc5d76caeeda202e228d37adb06f82195bf
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2024-06-21 17:16:04 +0530

    Update Q6.java

--- Lab Exam IMP Questions/Q6.java
+++ Lab Exam IMP Questions/Q6.java
@@ -3,6 +3,7 @@ and third thread prints prime numbers from 201 to 300. Note: The three threads m
 class primeNumber extends Thread {
     int start;
     int end;
+    static Object lock = new Object();
 
     primeNumber(int start, int end) {
         this.start = start;
@@ -10,16 +11,18 @@ class primeNumber extends Thread {
     }
 
     void generatePrime() {
-        for (int i = start; i <= end; i++) {
-            boolean flag = true;
-            for (int j = 2; j <= Math.sqrt(i); j++) {
-                if (i % j == 0) {
-                    flag = false;
-                    break;
+        synchronized(lock){
+            for (int i = start; i <= end; i++) {
+                boolean flag = true;
+                for (int j = 2; j <= Math.sqrt(i); j++) {
+                    if (i % j == 0) {
+                        flag = false;
+                        break;
+                    }
                 }
-            }
-            if (flag) {
-                System.out.println(i);
+                if (flag) {
+                    System.out.println(i);
+                }
             }
         }
     }
@@ -35,22 +38,7 @@ public class Q6 {
         primeNumber t2 = new primeNumber(101, 200);
         primeNumber t3 = new primeNumber(201, 300);
         t1.start();
-        try {
-            t1.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         t2.start();
-        try {
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         t3.start();
-        try {
-            t3.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
