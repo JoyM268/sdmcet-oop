0627b91cdbc48f584adc082065c7660d784d57c5
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-06-28 21:12:15 +0530

    Add Files

--- IA Answers/IA - 3 Jun 2024/Q3_a.java
+++ IA Answers/IA - 3 Jun 2024/Q3_a.java
@@ -9,4 +9,37 @@ Consider the following table available in the MySQL instance of BookDB
 | Publisher | varchar(30) |
 | Price     | real        |
 +-----------+-------------+
-*/
+Write a Java program that displays contents of the above-mentioned table on the console. */
+import java.sql.*;
+
+class Q3_a {
+	public static void main(String[] args) {
+		final String driver = "com.mysql.jdbc.Driver";
+		final String url = "jdbc:mysql://localhost:3306/BookDB";
+		final String userName = "root";
+		final String password = "";
+		
+		try {
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(url, userName, password);
+			if(conn == null) {
+				System.out.println("Connection could not be established");
+			}
+			
+			Statement stmt = conn.createStatement();
+			
+			String sql = "SELECT * FROM BookInfo";
+			ResultSet rs = stmt.executeQuery(sql);
+			System.out.println("The contents of the table are:");
+			while(rs.next()) {
+				//System.out.println(rs.getInt("ISBN") + " " + rs.getString("Title") + " " + rs.getString("Author") + " " + rs.getString("Publisher") + rs.getFloat("Price"));
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4) + " " + rs.getFloat(5));
+			}
+			conn.close();
+			stmt.close();
+			rs.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
